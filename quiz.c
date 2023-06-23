589ab0c6e1f59083c3b1cefef0d846921c960685
GAURAV KUMAR <96326330+gauravkumar2002@users.noreply.github.com>
2023-06-23 15:08:44 +0530

    Add files via upload

---
+++ quiz.c
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name,opt;
+    printf("\t\t\t\t\t\t WELCOME IN THE QUIZ COMPETITION");
+    printf("\n");
+    printf("ENTER YOUR NAME-");
+    scanf("%s",&name);
+    printf("Q1- which animal is known as the ship of the desert");
+    printf("\n");
+    printf("a-lion\t\t\t b- camel\nc-tiger\t\t\t d-dog");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='b')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q2- how many days are there in a week");
+    printf("\n");
+    printf("a-8week\t\t\t b- 10week\nc- 9week\t\t\t d-7week");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    
+    if (opt=='d')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q3-how many days are there in a year");
+    printf("\n");
+    printf("a-765\t\t\t b- 367\nc- 365\t\t\t d-246");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='c')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q4-how many minutes are there in an hour");
+    printf("\n");
+    printf("a-30min\t\t\t b-60min\nc- 20min\t\t\t d-40min");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='b')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q5-baby frog is known as ?");
+    printf("\n");
+    printf("a-tadpole\t\t\t b- bullfrog\nc- kitten\t\t\t d-calf");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='a')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q6-how many consonent are there in the english alphabet");
+    printf("\n");
+    printf("a-21\t\t\t b- 22\nc- 23\t\t\t d-24");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='a')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q7-which bird is known as national bird of india");
+    printf("\n");
+    printf("a-eagle\t\t\t b- mosquito\nc- peacock\t\t\t d- none of the above");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='c')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    
+printf("\n");
+    printf("Q8-The national flower of india is-");
+    printf("\n");
+    printf("a-lotus\t\t\t b- rose\nc- lily\t\t\t d-sunflower");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='a')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q9-the national river of india is-");
+    printf("\n");
+    printf("a-yamuna river\t\t\t b- ganga river\nc- narmada river\t\t\t d- godaveri river");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='b')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    printf("\n");
+    printf("Q10-what is the largest mamal on earth");
+    printf("\n");
+    printf("a-elephant\t\t\t b- giraff\nc- blue wwale\t\t\t d- none of the above");
+    printf("\n");
+    printf("Enter the correct option-");
+    scanf("%s",&opt);
+    if (opt=='c')
+    {
+       printf("correct");
+    }
+    else
+    {
+        printf("your answer is wrong");
+    }
+    
+    
+    
+    getch();
+    
+    
+
+
+}
